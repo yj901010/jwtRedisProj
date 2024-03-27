00e52a5ebee443c636b7135a8d768e2d80f8f439
yj901010 <lyj960107@gmail.com>
2024-03-27 11:16:07 +0900

    Initial commit

---
+++ src/main/java/edu/prac/jwtredisproj/JwtRedisProjApplication.java
@@ -0,0 +1,13 @@
+package edu.prac.jwtredisproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRedisProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRedisProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/prac/jwtredisproj/JwtRedisProjApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prac.jwtredisproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRedisProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
